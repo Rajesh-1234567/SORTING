d94399b1ddf69630f8935afc7dc2f82e4aec4a21
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2023-11-24 21:57:59 +0530

    MERGE_SORT.java

--- MERGE_SORT.java
+++ MERGE_SORT.java
@@ -1,1 +1,66 @@
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        mergeSort(array);
+
+        System.out.println("\nSorted array:");
+        printArray(array);
+    }
+
+    // Merge sort function
+    public static void mergeSort(int[] array) {
+        int n = array.length;
+        if (n > 1) {
+            int mid = n / 2;
+            int[] left = new int[mid];
+            int[] right = new int[n - mid];
+
+            // Populate left and right arrays
+            System.arraycopy(array, 0, left, 0, mid);
+            System.arraycopy(array, mid, right, 0, n - mid);
+
+            // Recursively sort the two halves
+            mergeSort(left);
+            mergeSort(right);
+
+            // Merge the sorted halves
+            merge(array, left, right);
+        }
+    }
+
+    // Merge function
+    public static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+
+        // Copy remaining elements of left and right arrays if any
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+    // Utility function to print an array
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
 
